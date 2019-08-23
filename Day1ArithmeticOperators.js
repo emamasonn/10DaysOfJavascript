a27008ac66c037bcec0233738d019f19a82fea78
David Mason <emamasonn@gmail.com>
2019-08-23 08:43:58 -0300

    Day 1: Arithmetic operators

---
+++ Day1ArithmeticOperators.js
@@ -0,0 +1,13 @@
+//https://www.hackerrank.com/challenges/js10-arithmetic-operators/problem
+
+function getArea(length, width) {
+    let area;
+    area = length * width;
+    return area;
+}
+
+function getPerimeter(length, width) {
+    let perimeter;
+    perimeter = 2*(length + width);
+    return perimeter;
+}
